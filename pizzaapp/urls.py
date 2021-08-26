d95e4e063aa04171c1d5e723853c83d34fc93438
adesh-mhetre <adeshmhetre143@gmail.com>
2021-08-26 11:53:32 +0530

    First Commit

---
+++ pizzaapp/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    #path('urlname',view),
+    path('admin/',adminloginview, name = 'adminloginpage'),
+    path('authenticateadmin/',authenticateadmin),
+    path('admin/homepage',adminhomepageview,name = 'adminhomepage'),
+    path('adminlogout/',logoutadmin),
+    path('addpizza/',addpizza),
+    path('deletepizza/<int:pizzapk>/',deletepizza),
+    path('',homepageview,name='homepage'),
+    path('signupuser/',signupuser),
+    path('loginuser/',userloginview,name="userloginpage"),
+    path('customer/welcome/',customerwelcomeview,name='customerpage'),
+    path('customer/authenticate/',userauthenticate),
+    path('userlogout/',userlogout),
+    path('placeorder/',placeorder),
+    path('userorders/',userorders),
+    path('adminorders/',adminorders),
+    path('acceptorder/<int:orderpk>/',acceptorder),
+    path('declineorder/<int:orderpk>/',declineorder),
+]
